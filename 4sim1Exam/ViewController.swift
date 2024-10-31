a71850f798e82f7cb9071d65618504d5c6d12a91
ElyesD1 <elyes.darouich@esprit.tn>
2024-10-31 12:51:40 +0100

    Initial Commit

---
+++ 4sim1Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  4sim1Exam
+//
+//  Created by Elyes Darouich on 31/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 4sim1ExamTests/_sim1ExamTests.swift
@@ -0,0 +1,17 @@
+//
+//  _sim1ExamTests.swift
+//  4sim1ExamTests
+//
+//  Created by Elyes Darouich on 31/10/2024.
+//
+
+import Testing
+@testable import _sim1Exam
+
+struct _sim1ExamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
